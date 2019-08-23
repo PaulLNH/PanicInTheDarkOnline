3ffb28f84d5a18f087f5781e765146b956e29e22
plaird730 <plaird730@gmail.com>
2019-08-22 21:37:48 -0400

    Merged the auth server and the game into one repo, fixed some dependencies and handlebars config for nodemailer

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,48 +1,181 @@
-let game, scores;
-
-class Highscore extends Phaser.Scene {
-
+class BootScene extends Phaser.Scene {
   constructor() {
     super({
-      key: 'Highscore',
+      key: 'BootScene',
       active: true
     });
   }
 
   preload() {
-    this.load.bitmapFont('arcade', 'assets/arcade.png', 'assets/arcade.xml');
+    // map tiles
+    this.load.image('tiles', 'assets/map/spritesheet-extruded.png');
+    // map in json format
+    this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+    // our two characters
+    this.load.spritesheet('player', 'assets/RPG_assets.png', {
+      frameWidth: 16,
+      frameHeight: 16
+    });
   }
 
   create() {
-    this.add.bitmapText(100, 110, 'arcade', 'RANK  SCORE   NAME').setTint(0xffffff);
+    this.scene.start('WorldScene');
+  }
+}
 
-    for (let i = 1; i < 6; i++) {
-      if (scores[i-1]) {
-        this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ${i}      ${scores[i-1].highScore}    ${scores[i-1].name}`).setTint(0xffffff);
-      } else {
-        this.add.bitmapText(100, 160 + 50 * i, 'arcade', ` ${i}      0    ---`).setTint(0xffffff);
-      }
+class WorldScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'WorldScene'
+    });
+  }
+
+  create() {
+    // create the map
+    var map = this.make.tilemap({
+      key: 'map'
+    });
+
+    // first parameter is the name of the tilemap in tiled
+    var tiles = map.addTilesetImage('spritesheet', 'tiles', 16, 16, 1, 2);
+
+    // creating the layers
+    var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+    var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+
+    // make all tiles in obstacles collidable
+    obstacles.setCollisionByExclusion([-1]);
+
+    //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [1, 7, 1, 13]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // animation with key 'right'
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [1, 7, 1, 13]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [2, 8, 2, 14]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [0, 6, 0, 12]
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // our player sprite created through the phycis system
+    this.player = this.physics.add.sprite(50, 100, 'player', 6);
+
+    // don't go out of the map
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+    this.player.setCollideWorldBounds(true);
+
+    // don't walk on trees
+    this.physics.add.collider(this.player, obstacles);
+
+    // limit camera to map
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.roundPixels = true; // avoid tile bleed
+
+    // user input
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // where the enemies will be
+    this.spawns = this.physics.add.group({
+      classType: Phaser.GameObjects.Zone
+    });
+    for (var i = 0; i < 30; i++) {
+      var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+      // parameters are x, y, width, height
+      this.spawns.create(x, y, 20, 20);
+    }
+    // add collider
+    this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
+  }
+
+  onMeetEnemy(player, zone) {
+    // we move the zone to some other location
+    zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+    zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+  }
+
+  update() {
+    // this.controls.update(delta);
+
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown) {
+      this.player.body.setVelocityX(-80);
+    } else if (this.cursors.right.isDown) {
+      this.player.body.setVelocityX(80);
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown) {
+      this.player.body.setVelocityY(-80);
+    } else if (this.cursors.down.isDown) {
+      this.player.body.setVelocityY(80);
+    }
+
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (this.cursors.left.isDown) {
+      this.player.anims.play('left', true);
+      this.player.flipX = true;
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play('right', true);
+      this.player.flipX = false;
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play('up', true);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play('down', true);
+    } else {
+      this.player.anims.stop();
     }
   }
 }
 
-let config = {
+var config = {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
-  width: 800,
-  height: 600,
+  parent: 'content',
+  width: 320,
+  height: 240,
+  zoom: 3,
   pixelArt: true,
-  scene: [Highscore]
-};
-
-$.ajax({
-  type: 'GET',
-  url: '/scores',
-  success: function(data) {
-    game = new Phaser.Game(config);
-    scores = data;
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0
+      },
+      debug: true // set to true to view zones
+    }
   },
-  error: function(xhr) {
-    console.log(xhr);
-  }
-});
+  scene: [
+    BootScene,
+    WorldScene
+  ]
+};
+var game = new Phaser.Game(config);
